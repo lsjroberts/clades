5b63bd69965ce5032311d02161d74eb4006db693
Laurence Roberts <lsjroberts@outlook.com>
2014-05-31 12:36:35 +0100

    Fixes and api connection

--- app/controllers/TaxonsShowController.js
+++ app/controllers/TaxonsShowController.js
@@ -1,7 +1,22 @@
 app.controller('TaxonsShowController', function($scope, $stateParams, taxonsFactory) {
-    render.taxons.redraw();
+    // render.taxons.redraw();
 
-    $scope.taxons = []
-        .concat([taxonsFactory.getByID($stateParams.id)])
-        .concat(taxonsFactory.getChildren($stateParams.id));
+    // $scope.taxons = []
+    //     .concat([taxonsFactory.getByID($stateParams.id)])
+    //     .concat(taxonsFactory.getChildren($stateParams.id));
+
+    $scope.taxons = [];
+
+    taxonsFactory.getByID($stateParams.id, { children: true })
+        .success(function(response) {
+            $scope.taxons.push(response.data);
+            for (var child in response.data.children.data) {
+                $scope.taxons.push(response.data.children.data[child]);
+            }
+
+            console.log($scope.taxons);
+        })
+        .error(function(response) {
+
+        });
 });
--- app/factories/TaxonsFactory.js
+++ app/factories/TaxonsFactory.js
@@ -1,43 +1,36 @@
 app.factory('taxonsFactory', function($http) {
-    var factory = {};
-
-    factory.testData = [
-        {id: 1, parentID: null, name: "Life", rank: "life", extant: 1},
-        {id: 2, parentID: 1, name: "Bacteria", rank: "domain", extant: 1},
-        {id: 3, parentID: 1, name: "Archaea", rank: "domain", extant: 1},
-        {id: 4, parentID: 1, name: "Eukaryota", rank: "domain", extant: 1},
-        {id: 5, parentID: 4, name: "Animalia", rank: "kingdom", extant: 1},
-        {id: 6, parentID: 4, name: "Plantae", rank: "kingdom", extant: 1},
-        {id: 7, parentID: 4, name: "Chromaleveolata", rank: "kingdom", extant: 1},
-        {id: 8, parentID: 4, name: "Rhizaria", rank: "kingdom", extant: 1},
-        {id: 9, parentID: 4, name: "Excavata", rank: "kingdom", extant: 1},
-    ];
+    var factory = {},
+        root = 'http://localhost:8001';
 
     factory.getAll = function(id) {
-        return this.testData;
+        return $http.get(root + '/taxa');
     };
 
-    factory.getByID = function(id) {
-        taxons = this.getAll();
-        for (i in taxons) {
-            if (taxons[i].id == id) {
-                return taxons[i];
-            }
+    factory.getByID = function(id, options) {
+        var settings = this._unionSettings({ children: false }, options),
+            path = root + '/taxa/' + id;
+
+        if (settings.children) {
+            path += '?embed=children';
         }
 
-        return false;
+        return $http.get(path);
     };
 
     factory.getChildren = function(parentID) {
-        taxons = this.getAll();
-        children = [];
-        for (i in taxons) {
-            if (taxons[i].parentID == parentID) {
-                children.push(taxons[i]);
+        return $http.get(root + '/taxa/' + parentID + '/children');
+    }
+
+    factory._unionSettings = function(settings, options) {
+        if (typeof options != 'undefined') {
+            for (var k in settings) {
+                if (typeof options[k] != 'undefined') {
+                    settings[k] = options[k];
+                }
             }
         }
 
-        return children;
+        return settings;
     }
 
     return factory;
